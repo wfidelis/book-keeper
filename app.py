103d61aa182b9939d0be0905d4484f9ef0080fa9
wfidelis <mainafidelis90@gmail.com>
2022-09-07 16:17:26 +0300

    initial commit

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
 
 #SQLAlchemy Database configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:*mMN7FnL0]{t@localhost/booksdb'
-app.config['SECRET_KEY'] = "x10\xcbb\x00\x94\xccA\xc8\x13\xfc\x89K"
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.getenv('SQLALCHEMY_TRACK_MODIFICATIONS')
 db = SQLAlchemy(app)
 
 
